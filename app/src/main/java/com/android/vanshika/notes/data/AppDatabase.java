a9efd7940a47679a9c124d0dd4cf27b32c13b0e9
vanshikaarora <34261945+vanshikaarora@users.noreply.github.com>
2018-08-07 17:29:04 +0530

    Update AppDatabase.java

--- app/src/main/java/com/android/vanshika/notes/data/AppDatabase.java
+++ app/src/main/java/com/android/vanshika/notes/data/AppDatabase.java
@@ -5,7 +5,7 @@ import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
 
-//sir yeh tjeek h?kahan gaye sir aap?
+
 @Database(entities = {user.class,myuser.class}, version = 2)
 public abstract class AppDatabase extends RoomDatabase {
   private static AppDatabase INSTANCE;
@@ -19,6 +19,3 @@ public abstract class AppDatabase extends RoomDatabase {
     }return INSTANCE;
   }
 }
-// humare kotlin main to directly array ban jata hei, isme kaise banega?yeh toh mujhe bhi nhi ppta sir mujhe c++ zyaada aati h as compared to java. ok ma'am
-//i sec sir muje yaad aa gaya java mein array k liye array variable={"first string","second string"}; bhia upar kar de change jo bhi hoga.
-//nhi ho rha yeh naya object bna k kese karte hain yaad nhi
